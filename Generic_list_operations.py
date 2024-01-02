6a873448da544035356103219d4c181c3fa36ade
Tau <lorenzo.tausani@studenti.unipd.it>
2024-01-02 22:48:24 +0100

    multioption prompt

--- Generic_list_operations.py
+++ Generic_list_operations.py
@@ -1,4 +1,4 @@
-from typing import List, Callable, Any
+from typing import List, Callable, Any, Union
 
 def filter_list(input_list: List[Any], condition_func: Callable[[Any], bool]) -> List[Any]:
     """
@@ -12,3 +12,30 @@ def filter_list(input_list: List[Any], condition_func: Callable[[Any], bool]) ->
     - filtered_list: Lista risultante dopo il filtro.
     """
     return [item for item in input_list if condition_func(item)]
+
+
+def multioption_prompt(opt_list: List[str], in_prompt: str) -> Union[str, List[str]]:
+    """
+    Prompt the user to choose from a list of options.
+
+    Parameters:
+    - opt_list: List of options.
+    - in_prompt: Prompt message to display.
+
+    Returns:
+    - Either a single option (str) or a list of options (List[str]).
+    """
+    # Generate option list
+    opt_prompt = '\n'.join([f'{i}: {opt}' for i, opt in enumerate(opt_list)])
+    
+    # Prompt user and evaluate input
+    idx_answer = eval(input(f"{in_prompt}\n{opt_prompt}"))
+    
+    # Check if the answer is a list
+    if isinstance(idx_answer, list):
+        answer = [opt_list[idx] for idx in idx_answer]
+    else:
+        # If not a list, return the corresponding option
+        answer = opt_list[idx_answer]
+
+    return answer
