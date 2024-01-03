481f7209e6183e04a10d6bb1999dde2d6cb2cf68
Tau <lorenzo.tausani@studenti.unipd.it>
2024-01-03 18:53:19 +0100

    SEMf definita seguendo analisi Lodo

--- Generic_numeric_operations.py
+++ Generic_numeric_operations.py
@@ -1,13 +1,14 @@
-import numpy as np
 from typing import Union, List, Tuple
 from Generic_converters import *
+import numpy as np
 
-def SEM(data: Union[List[float], Tuple[float], np.ndarray]) -> float:
+def SEMf(data: Union[List[float], Tuple[float], np.ndarray], axis: int = 0) -> float:
     """
     Calcola lo standard error of the mean (SEM) per una sequenza di numeri.
 
     Parameters:
     - data: Sequenza di numeri (lista, tupla, array NumPy, ecc.).
+    - axis: Asse lungo il quale calcolare la deviazione standard (valido solo per array NumPy).
 
     Returns:
     - sem: Standard error of the mean (SEM).
@@ -16,9 +17,9 @@ def SEM(data: Union[List[float], Tuple[float], np.ndarray]) -> float:
         # Converte la sequenza in un array NumPy utilizzando la funzione convert_to_numpy
         data_array = convert_to_numpy(data)
         
-        # Calcola la deviazione standard e il numero di campioni
-        std_dev = np.std(data_array, ddof=1)  # ddof=1 per la stima corretta nella popolazione
-        sample_size = len(data_array)
+        # Calcola la deviazione standard e il numero di campioni specificando l'asse se necessario
+        std_dev = np.nanstd(data_array, axis=axis) if data_array.ndim > 1 else np.nanstd(data_array)
+        sample_size = data_array.shape[axis]
         
         # Calcola lo standard error of the mean (SEM)
         sem = std_dev / np.sqrt(sample_size)
